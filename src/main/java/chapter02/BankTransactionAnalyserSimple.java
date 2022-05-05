699c6a77d9dd74d346b3e1a2ed50b06b8ad27915
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-05-05 14:58:25 +0200

    bad code duplication

--- src/main/java/chapter02/BankTransactionAnalyserSimple.java
+++ src/main/java/chapter02/BankTransactionAnalyserSimple.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class BankTransactionAnalyserSimple {
@@ -19,5 +22,17 @@ public class BankTransactionAnalyserSimple {
             total += amount;
         }
         System.out.println("The total for all transactions is " + total);
+
+        total = 0;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        for (final String line : lines){
+            final String[] columns = line.split(",");
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+            if (date.getMonth() == Month.JANUARY){
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
+        }
+        System.out.println("The total for all transactions in January is " + total);
     }
 }
