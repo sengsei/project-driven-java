c555ea10a798df14cbdfe54093665ebc6d509176
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-05-07 11:08:59 +0200

    domain class for bank transaction

---
+++ src/main/java/chapter02/BankStatementCSVParser.java
@@ -0,0 +1,10 @@
+package chapter02;
+
+import java.time.format.DateTimeFormatter;
+
+public class BankStatementCSVParser {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+
+}
