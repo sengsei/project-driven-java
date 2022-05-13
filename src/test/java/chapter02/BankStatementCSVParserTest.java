7d4b2d4ae96804de21912a79d47d50978ea94456
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-05-13 10:14:39 +0200

    adding tests for calc class

--- src/test/java/chapter02/BankStatementCSVParserTest.java
+++ src/test/java/chapter02/BankStatementCSVParserTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
