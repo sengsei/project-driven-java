c4069028591f50ae13700f4bb8a59c404ead8878
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-05-23 10:22:11 +0200

    import Images

---
+++ src/main/java/chapter04/Attributes.java
@@ -0,0 +1,8 @@
+package chapter04;
+
+public final class Attributes {
+    public static final String PATH = "path";
+    public static final String WIDTH = "width";
+    public static final String HEIGHT = "height";
+    public static final String TYPE = "type";
+}
