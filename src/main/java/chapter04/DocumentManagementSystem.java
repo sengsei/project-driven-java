c5bb8dd15df5c4b0b9c7b5b32640e6ba9bdfb0e9
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-05-23 10:45:37 +0200

    DM system

---
+++ src/main/java/chapter04/DocumentManagementSystem.java
@@ -0,0 +1,41 @@
+package chapter04;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+
+public class DocumentManagementSystem {
+    private final List<Document> documents = new ArrayList<>();
+    private final List<Document> documentsView = Collections.unmodifiableList(documents);
+    private final Map<String, Importer> extensionToImporter = new HashMap<>();
+
+    public DocumentManagementSystem() {
+        extensionToImporter.put("jpg", new ImageImporter());
+    }
+
+    public void importFile(final String path) throws IOException{
+        final File file = new File(path);
+        if (!file.exists()){
+            throw new FileNotFoundException(path);
+        }
+
+        final int separatorIndex = path.lastIndexOf('.');
+        if (separatorIndex != -1) {
+            if (separatorIndex == path.length()) {
+                throw new UnknownFileTypeException("No extension found For file: " + path);
+            }
+            final String extension = path.substring(separatorIndex + 1);
+            final Importer importer = extensionToImporter.get(extension);
+            if (importer == null) {
+                throw new UnknownFileTypeException("For file: " + path);
+            }
+
+            final Document document = importer.importFile(file);
+            documents.add(document);
+        } else {
+            throw new UnknownFileTypeException("No extension found For file: " + path);
+        }
+
+    }
+}
---
+++ src/main/java/chapter04/UnknownFileTypeException.java
@@ -0,0 +1,7 @@
+package chapter04;
+
+public class UnknownFileTypeException extends RuntimeException{
+    public UnknownFileTypeException(final String message){
+        super(message);
+    }
+}
